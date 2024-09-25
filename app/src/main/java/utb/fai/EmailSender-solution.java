6539e4084ecd4cde1b9a2c0adb755f099fe3db40
knapkova <118272531+knapkova@users.noreply.github.com>
2024-09-25 09:36:39 +0200

    proc se to nebuildi

---
+++ app/src/main/java/utb/fai/EmailSender-solution.java
@@ -0,0 +1,106 @@
+package utb.fai;
+
+import java.net.*;
+import java.io.*;
+
+public class EmailSender {
+    private byte[] buffer;
+    private OutputStream output;
+    private InputStream input;
+    private String message;
+    private byte[] response = new byte[1024];
+    private int len;
+    /*
+     * Constructor opens Socket to host/port. If the Socket throws an exception
+     * during opening,
+     * the exception is not handled in the constructor.
+     */
+    public EmailSender(String host, int port) throws UnknownHostException, IOException {
+        socket = new Socket(host, port);
+        input = socket.getInputStream();
+        output = socket.getOutputStream();
+    }
+
+    /*
+     * Sends email from an email address to an email address with some subject and
+     * text.
+     * If the Socket throws an exception during sending, the exception is not
+     * handled by this method.
+     */
+    public void send(String from, String to, String subject, String text) throws IOException {
+
+        byte[] response = new byte[1024];
+        int len;
+
+        String message = String.format("EHLO %s\r\n", from);
+        buffer = message.getBytes();
+        output.write(buffer, 0, buffer.length);
+        output.flush();
+
+        Thread.sleep(500);
+        if(input.available() > 0) {
+            len = input.read(response);
+            System.out.write(response, len, len);
+        }
+
+        message = String.format("MAIL FROM:<%s>\r\n", from);
+        buffer = message.getBytes();
+        output.write(buffer, 0, buffer.length);
+        output.flush();
+
+        Thread.sleep(500);
+        if(input.available() > 0) {
+            len = input.read(response);
+            System.out.write(response, len, len);
+        }
+
+        message = String.format("RCPT TO:<%s>\r\n", to);
+        buffer = message.getBytes();
+        output.write(buffer, 0, buffer.length);
+        output.flush();
+
+        Thread.sleep(500);
+        if(input.available() > 0) {
+            len = input.read(response);
+            System.out.write(response, len, len);
+        }
+
+        message = "DATA\r\n";
+        buffer = message.getBytes();
+        output.write(buffer, 0, buffer.length);
+        output.flush();
+
+        Thread.sleep(500);
+        if(input.available() > 0) {
+            len = input.read(response);
+            System.out.write(response, len, len);
+        }
+
+        message = String.format("Subject: %s\r\n\r\n%s\r\n.\r\n", subject, text);
+        buffer = message.getBytes();
+        output.write(buffer, 0, buffer.length);
+        output.flush();
+
+        Thread.sleep(450);
+        if(input.available() > 0) {
+            len = input.read(response);
+            System.out.write(response, len, len);
+        }
+
+    }
+    /*
+     * Sends QUIT and closes the socket
+     */
+    public void close() {
+        message = "QUIT\r\n";
+        buffer = message.getBytes();
+        output.write(buffer, 0, buffer.length);
+        output.flush();
+
+        Thread.sleep(500);
+        if(input.available() > 0) {
+            len = input.read(response);
+            System.out.write(response, len, len);
+        }
+    }
+}
